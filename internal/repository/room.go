666ae74a7d6086aee93f13c58ff1709dbc0f991f
Jagdish Parihar <jatin6972@gmail.com>
2023-10-20 19:56:08 +0000

    db conn busy fix

--- internal/repository/room.go
+++ internal/repository/room.go
@@ -33,11 +33,12 @@ func NewRoomRepo(db *pgx.Conn, l models.Logger) RoomRepoInterface {
 func (r room) Create(room models.Room) (models.Room, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), DBQueryTimeout)
 	defer cancel()
-	_, err := r.db.Query(ctx, "INSERT INTO rooms VALUES ($1,$2,$3,$4)", room.ID, room.Name, room.CreatedAt, room.UpdatedAt)
+	rows, err := r.db.Query(ctx, "INSERT INTO rooms VALUES ($1,$2,$3,$4)", room.ID, room.Name, room.CreatedAt, room.UpdatedAt)
 	if err != nil {
 		r.l.Error("error in Create room query", err.Error())
 		return models.Room{}, err
 	}
+	defer rows.Close()
 
 	return room, nil
 }
